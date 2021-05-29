51609e25f25b1196be4becb4330a731b11c14fc5
Ricky Aristya <aristyaricky@gmail.com>
2021-05-29 08:26:01 +0700

    menambahkan file gsap dan fix animation

---
+++ js/script-gsap.js
@@ -0,0 +1,7 @@
+gsap.from(".jepun", { duration: 1.5, x: -100, opacity: 0, delay: 4.5 });
+gsap.from(".home-text h1", { duration: 1.5, x: -100, opacity: 0, delay: 5 });
+gsap.from(".home-text h2", { duration: 1.5, x: -100, opacity: 0, delay: 5.2 });
+gsap.from(".home-text .btn", { duration: 1.5, x: -100, opacity: 0, delay: 5.5 });
+gsap.from(".home-img", { duration: 1.5, x: 100, opacity: 0, delay: 5.8 });
+gsap.from(".nav-toggler", { duration: 1.5, opacity: 0, delay: 5 });
+gsap.from(".day-mode", { duration: 1.5, opacity: 0, delay: 6.5 });
