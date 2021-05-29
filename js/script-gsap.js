551c51f2ca983315e8c47de567d348d5b348d88a
Ricky Aristya <aristyaricky@gmail.com>
2021-05-29 12:15:49 +0700

    edit file dan fix bug

--- js/script-gsap.js
+++ js/script-gsap.js
@@ -4,4 +4,4 @@ gsap.from(".home-text h2", { duration: 1.5, x: -100, opacity: 0, delay: 5.2 });
 gsap.from(".home-text .btn", { duration: 1.5, x: -100, opacity: 0, delay: 5.5 });
 gsap.from(".home-img", { duration: 1.5, x: 100, opacity: 0, delay: 5.8 });
 gsap.from(".nav-toggler", { duration: 1.5, opacity: 0, delay: 5 });
-gsap.from(".day-mode", { duration: 1.5, opacity: 0, delay: 6.5 });
+gsap.from(".day-mode", { duration: 1.5, opacity: 0, delay: 6 });
